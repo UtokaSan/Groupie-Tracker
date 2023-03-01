9782f4bebdc76e7e93c4a87ef94b5d920df0e461
maxime <maxime.poiret@ynov.com>
2023-03-01 11:26:01 +0100

    update: javascript+html

--- templates/assets/scripts/index.js
+++ templates/assets/scripts/index.js
@@ -1,10 +1,19 @@
 const rapButton = document.getElementById("rap-button");
+const popVibesButton = document.getElementById("vibes-button");
+
 
 rapButton.addEventListener('click', function() {
     let urlParameter = new URLSearchParams();
-
     urlParameter.append('genre', 'rap');
 
-    let urlNewPage = `http://localhost:8080/categorie?${urlParameter.toString()}`
+    let urlNewPage = `http://localhost:3001/categorie?${urlParameter.toString()}`
     window.location.href = urlNewPage
+});
+
+popVibesButton.addEventListener('click', function() {
+    let urlParameterPop = new URLSearchParams();
+    urlParameterPop.append('genre', 'Pop-vibes');
+
+    let urlNewPagePop = `http://localhost:3001/categorie?${urlParameterPop.toString()}`
+    window.location.href = urlNewPagePop
 });
