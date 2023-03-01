06bf4c8bd17ae3d4ae6e86df679ec26e012247b9
maxime <maxime.poiret@ynov.com>
2023-03-01 14:39:59 +0100

    update: javascript + html

--- templates/assets/scripts/index.js
+++ templates/assets/scripts/index.js
@@ -1,6 +1,8 @@
 const rapButton = document.getElementById("rap-button");
 const popVibesButton = document.getElementById("vibes-button");
-
+const electrobutton = document.getElementById("electro-button");
+const rockbutton = document.getElementById("rock-button");
+const metalbutton = document.getElementById("metal-button");
 
 rapButton.addEventListener('click', function() {
     let urlParameter = new URLSearchParams();
@@ -16,4 +18,26 @@ popVibesButton.addEventListener('click', function() {
 
     let urlNewPagePop = `http://localhost:3001/categorie?${urlParameterPop.toString()}`
     window.location.href = urlNewPagePop
+});
+
+electrobutton.addEventListener('click', function() {
+    let urlParameterElectro = new URLSearchParams();
+    urlParameterElectro.append('genre', 'Electro');
+
+    let urlNewPageElectro = `http://localhost:3001/categorie?${urlParameterElectro.toString()}`
+    window.location.href = urlNewPageElectro
+});
+rockbutton.addEventListener('click', function() {
+    let urlParameterRock = new URLSearchParams();
+    urlParameterRock.append('genre', 'Rock');
+
+    let urlNewPageRock = `http://localhost:3001/categorie?${urlParameterRock.toString()}`
+    window.location.href = urlNewPageRock
+});
+metalbutton.addEventListener('click', function() {
+    let urlParameterMetal = new URLSearchParams();
+    urlParameterMetal.append('genre', 'Metal');
+
+    let urlNewPageMetal = `http://localhost:3001/categorie?${urlParameterMetal.toString()}`
+    window.location.href = urlNewPageMetal
 });
