803522f90d2d46a55d08598812da2a9cf4ed0407
maxime <maxime.poiret@ynov.com>
2023-03-02 22:44:47 +0100

    update: loding bar

--- templates/assets/scripts/artistinfo.js
+++ templates/assets/scripts/artistinfo.js
@@ -1,14 +1,3 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('genre');
-
-const data = {id};
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('/api/genre', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error))
-})
+setTimeout(function() {
+    window.location.href = "http://localhost:3001/categorie?genre=Pop-vibes";
+}, 10000);
