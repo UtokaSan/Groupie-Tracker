246263cb90c64ab2782bc9e848f04cf3c0580bd4
maxime <maxime.poiret@ynov.com>
2023-03-06 14:54:28 +0100

    update: categorie html + css + js

--- templates/assets/scripts/categorie.js
+++ templates/assets/scripts/categorie.js
@@ -1,6 +1,7 @@
 const progressBar = document.querySelector('.progress-bar');
 const progressId = document.getElementById("progress")
 
+document.querySelector(".disk").style.display = "none";
 function updateProgress(progress) {
     progressBar.style.width = progress + '%';
     progressBar.setAttribute('aria-valuenow', progress);
@@ -8,9 +9,10 @@ function updateProgress(progress) {
 
 var progress = 100;
 var interval = setInterval(function() {
-    progress -= 12.8; // Diminuer de 10%
+    progress -= 10.0; // Diminuer de 10%
     updateProgress(progress);
     if (progress <= 0) {
         progressId.style.display = "none";
+        document.querySelector(".disk").style.display = "block";
     }
 }, 1000);
