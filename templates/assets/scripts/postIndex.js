a1aa46a96d8816e4c38140e7bf1480551a62471e
Florian <florian.boulbes@ynov.com>
2023-03-07 11:52:13 +0100

    update: suggestion & searchbar js

--- templates/assets/scripts/postIndex.js
+++ templates/assets/scripts/postIndex.js
@@ -4,13 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             let valueSearch = document.getElementById("search-input")
             let button = document.querySelector(".search-button");
-            valueSearch.addEventListener("input", function() {
+           /* console.log(data.relation.datesLocations[0])*/
+            valueSearch.addEventListener("input", function(event) {
+                let suggestions = document.getElementById("suggestions");
+                suggestions.innerHTML = "";
                 data.artists.forEach(function (value, index) {
-                    if (valueSearch.value === value.name) {
-                        window.location.href = "http://localhost:8080/artistinfo?artist=damso"
+                    if (value.name.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
+                        let option = document.createElement("option");
+                        option.value = value.name + " -Artist";
+                        suggestions.appendChild(option);
                     }
                 });
-            })
+            });
+            valueSearch.addEventListener("input", function(event) {
+                data.artists.forEach(function (value, index) {
+                    if (event.target.value === value.name + " -Artist") {
+                        window.location.href = `http://localhost:8080/artistinfo?artist=${value.id}`;
+                    }
+                });
+            });
         })
         .catch(error => console.error(error))
 })
