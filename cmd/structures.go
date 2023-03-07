72f602235acd31c4f5d05820e7f507bc62343b2a
Florian <florian.boulbes@ynov.com>
2023-03-07 15:35:11 +0100

    update: searchbar can search artist of member member & artist

--- cmd/structures.go
+++ cmd/structures.go
@@ -1,10 +1,11 @@
 package cmd
 
 type ImageID struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Image string `json:"image"`
-	Genre string
+	ID      int      `json:"id"`
+	Name    string   `json:"name"`
+	Image   string   `json:"image"`
+	Members []string `json:"members"`
+	Genre   string
 }
 type Test struct {
 	Artists  []ImageID `json:"artists"`
--- templates/assets/scripts/postIndex.js
+++ templates/assets/scripts/postIndex.js
@@ -4,25 +4,62 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             let valueSearch = document.getElementById("search-input")
             let button = document.querySelector(".search-button");
-           /* console.log(data.relation.datesLocations[0])*/
+            /*console.log(data.relation.index[0].datesLocations);*/
             valueSearch.addEventListener("input", function(event) {
                 let suggestions = document.getElementById("suggestions");
                 suggestions.innerHTML = "";
-                data.artists.forEach(function (value, index) {
-                    if (value.name.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
+                data.artists.forEach(function (artist) {
+                    if (artist.name.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
                         let option = document.createElement("option");
-                        option.value = value.name + " -Artist";
+                        option.value = artist.name + " -Artist";
                         suggestions.appendChild(option);
                     }
+                    artist.members.forEach(function(member) {
+                        if (member.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
+                            let option = document.createElement("option");
+                            option.value = member + " -Member";
+                            suggestions.appendChild(option);
+                        }
+                    });
                 });
             });
+
             valueSearch.addEventListener("input", function(event) {
-                data.artists.forEach(function (value, index) {
-                    if (event.target.value === value.name + " -Artist") {
-                        window.location.href = `http://localhost:8080/artistinfo?artist=${value.id}`;
+                data.artists.forEach(function (artist) {
+                    if (event.target.value === artist.name + " -Artist") {
+                        window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
                     }
+                    artist.members.forEach(function(member) {
+                        if (event.target.value === member + " -Member") {
+                            window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
+                        }
+                    });
                 });
             });
+
+
+            /*           valueSearch.addEventListener("input", function(event) {
+                           let suggestions = document.getElementById("suggestions");
+                           suggestions.innerHTML = "";
+                           data.relation.index.forEach(function (value, index) {
+                               Object.keys(value.datesLocations).forEach(function(key) {
+                                   if (key.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
+                                       let option = document.createElement("option");
+                                       option.value = value.datesLocations[key] + " -Relation";
+                                       suggestions.appendChild(option);
+                                   }
+                               });
+                           });
+                       });
+                       valueSearch.addEventListener("input", function(event) {
+                           data.relation.index.forEach(function (value, index) {
+                               Object.keys(value.datesLocations).forEach(function(key) {
+                                   if (event.target.value === value.datesLocations[key] + " -Relation") {
+                                       window.location.href = `http://localhost:8080/artistinfo?artist=${value.id}`;
+                                   }
+                               });
+                           });
+                       });*/
         })
         .catch(error => console.error(error))
 })
