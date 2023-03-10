62dc5b21b6f25692db6a4277496105c345e76659
Florian <florian.boulbes@ynov.com>
2023-03-10 13:51:55 +0100

    update: searchbar

--- templates/assets/scripts/indexSearchbar.js
+++ templates/assets/scripts/indexSearchbar.js
@@ -1,3 +1,4 @@
+let option = document.createElement("option");
 document.addEventListener('DOMContentLoaded', function () {
     fetch('/post/searchbar')
         .then(response => response.json())
@@ -12,19 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 suggestions.innerHTML = "";
                 data.artists.forEach(function (artist) {
                     if (artist.name.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
-                        let option = document.createElement("option");
                         option.value = artist.name + " -Artist";
                         suggestions.appendChild(option);
                     }
                     artist.members.forEach(function (member) {
                         if (member.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
-                            let option = document.createElement("option");
                             option.value = member + " -Member";
                             suggestions.appendChild(option);
                         }
                     });
                     if (artist.creationDate.toString().startsWith(valueSearch.value.toLowerCase())) {
-                        let option = document.createElement("option");
                         option.value = artist.creationDate + " -Creation Date";
                         suggestions.appendChild(option);
                     }
@@ -59,30 +57,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     })
                 });
             })
-
-
-            /*           valueSearch.addEventListener("input", function(event) {
-                           let suggestions = document.getElementById("suggestions");
-                           suggestions.innerHTML = "";
-                           data.relation.index.forEach(function (value, index) {
-                               Object.keys(value.datesLocations).forEach(function(key) {
-                                   if (key.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
-                                       let option = document.createElement("option");
-                                       option.value = value.datesLocations[key] + " -Relation";
-                                       suggestions.appendChild(option);
-                                   }
-                               });
-                           });
-                       });
-                       valueSearch.addEventListener("input", function(event) {
-                           data.relation.index.forEach(function (value, index) {
-                               Object.keys(value.datesLocations).forEach(function(key) {
-                                   if (event.target.value === value.datesLocations[key] + " -Relation") {
-                                       window.location.href = `http://localhost:8080/artistinfo?artist=${value.id}`;
-                                   }
-                               });
-                           });
-                       });*/
         })
         .catch(error => console.error(error))
 })
