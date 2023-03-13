8673bedd3d79f09efbe2c78ba565d885fa29f6f7
Florian <florian.boulbes@ynov.com>
2023-03-13 21:05:13 +0100

    update: final searchbar work

--- templates/assets/scripts/indexSearchbar.js
+++ templates/assets/scripts/indexSearchbar.js
@@ -1,68 +1,77 @@
-let dataArtist = [];
 document.addEventListener('DOMContentLoaded', function () {
     fetch('/post/searchbar')
         .then(response => response.json())
         .then(data => {
-            let valueSearch = document.getElementById("search-input")
-            let button = document.querySelector(".search-button");
-            let takeFirstLetter = valueSearch.value.substring(0, 3)
-            /*console.log(data.relation.index[0].datesLocations);*/
+            let valueSearch = document.getElementById("search-input");
+            let suggestions = document.getElementById("suggestions");
+            let form = document.querySelector('.search-form');
             valueSearch.addEventListener("input", function(event) {
-                let suggestions = document.getElementById("suggestions");
                 suggestions.innerHTML = "";
                 if (valueSearch.value.length > 3) {
-                    data.artists.forEach(function (artist) {
+                    for (let artist of data.artists) {
                         if (artist.name.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
                             let option = document.createElement("option");
-                            option.value = artist.name + " -Artist";
+                            option.value = `${artist.name} -Artist`;
                             suggestions.appendChild(option);
                         }
-                        artist.members.forEach(function (member) {
+                        for (let member of artist.members) {
                             if (member.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
                                 let option = document.createElement("option");
+                                option.value = `${member} -Member`;
                                 suggestions.appendChild(option);
-                                option.value = member + " -Member";
                             }
-                        });
+                        }
                         if (artist.creationDate.toString().startsWith(valueSearch.value.toLowerCase())) {
                             let option = document.createElement("option");
-                            option.value = artist.creationDate + " - " + artist.name + " -Creation Date";
+                            option.value = `${artist.creationDate} - ${artist.name} -Creation Date`;
                             suggestions.appendChild(option);
                         }
-                        data.location.index.forEach(function (index) {
-                            index.locations.forEach(function (location) {
+
+                        for (let index of data.location.index) {
+                            for (let location of index.locations) {
+                                location = location.replace(/[-_]/g, ' ');
                                 if (location.toLowerCase().startsWith(valueSearch.value.toLowerCase())) {
+                                    let artist = data.artists.find(function (artist) {
+                                        return artist.id === index.id;
+                                    });
                                     let option = document.createElement("option");
-                                    option.value = artistName;
-                                    suggestions.appendChild(option);
+                                    option.value = `${location} - ${artist.name}`
+                                    if (!suggestions.querySelector(`option[value="${option.value}"]`)) {
+                                        suggestions.appendChild(option);
+                                    }
                                 }
-                            });
-                        });
-                    });
+                            }
+                        }
+                    }
                 }
-                valueSearch.addEventListener("input", function (event) {
-                    data.artists.forEach(function (artist) {
-                        if (event.target.value === artist.name + " -Artist") {
+            });
+
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                data.artists.forEach(function (artist) {
+                    if (valueSearch.value === artist.name + " -Artist") {
+                        window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
+                    }
+                    artist.members.forEach(function (member) {
+                        if (valueSearch.value === member + " -Member") {
                             window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
                         }
-                        artist.members.forEach(function (member) {
-                            if (event.target.value === member + " -Member") {
-                                window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
-                            }
-                        });
-                        if (event.target.value === artist.creationDate.toString() + " -Creation Date") {
-                                window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
-                        }
                     });
-                    data.location.index.forEach(function (index) {
-                        index.locations.forEach(function (location) {
-                            if (event.target.value === location + " -Artist") {
-                                window.location.href = `http://localhost:8080/artistinfo?artist=${index.id}`;
-                            }
-                        })
-                    })
+                    if (valueSearch.value === artist.creationDate.toString() + " -Creation Date") {
+                        window.location.href = `http://localhost:8080/artistinfo?artist=${artist.id}`;
+                    }
                 });
-            })
+                data.location.index.forEach(function (index) {
+                    index.locations.forEach(function (location) {
+                        location = location.replace(/[-_]/g, ' ');
+                        for (let artists of data.artists) {
+                            if (valueSearch.value === `${location} - ${artists.name}`) {
+                                window.location.href = `http://localhost:8080/artistinfo?artist=${artists.id}`;
+                            }
+                        }
+                    })
+                })
+            });
         })
         .catch(error => console.error(error))
-})
+});
