526358bfd96d11d54397fcb59baf95f67c34ca97
maxime <maxime.poiret@ynov.com>
2023-03-14 13:17:33 +0100

    update: artistinfo js

--- templates/assets/scripts/artistinfoGet.js
+++ templates/assets/scripts/artistinfoGet.js
@@ -1,6 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('artist');
 const data = {id}
+console.log("test")
 document.addEventListener('DOMContentLoaded', function () {
     fetch('/get/artistinfo', {
         method: 'POST',
@@ -9,8 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
     })
         .then(response => response.json())
         .then(data => {
-            document.querySelector(".title_paralax").texteContent = data.artist.name
+            document.querySelector(".title_paralax").textContent = data.artist.name
+
             document.querySelector(".about__p").textContent = data.artist.creationDate
+            console.log(data)
         })
         .catch(error => console.error(error))
 })
